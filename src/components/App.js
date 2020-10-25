0fe48f76b44b1bd1618d4f686f854013c85f3c95
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-10-25 01:29:25 -0400

    Add editor functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,53 @@
 
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Editor from './Editor'
 
 function App () {
+
+	const [html, setHtml] = useState('')
+	const [css, setCss] = useState('')
+	const [js, setJs] = useState('')
+	const [srcDoc, setSrcDoc] = useState('')
+
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setSrcDoc(`
+			<html>
+				<body>${html}</body>
+				<style>${css}</style>
+				<script>${js}</script>
+			</html>
+			`)
+		}, 250)
+
+		return () => clearTimeout(timeout)
+	}, [html, css, js])
+
 	return (
 		<>
 			<div className="pane top-pane">
 				<Editor 
 					language="xml"
 					displayName="HTML"
-					value={}
-					onChange={} 
+					value={html}
+					onChange={setHtml} 
 				/>
-				<Editor />
-				<Editor />
+				<Editor 
+					language="css"
+					displayName="CSS"
+					value={css}
+					onChange={setCss} 
+				/>
+				<Editor 
+					language="javascript"
+					displayName="JS"
+					value={js}
+					onChange={setJs} 
+				/>
+				
 			</div>
 			<div className="pane">
-				<iframe src="" frameborder="0" title="output" sandbox="allow-scripts" width="100%" height="100%"></iframe>
+				<iframe srcDoc={srcDoc} frameborder="0" title="output" sandbox="allow-scripts" width="100%" height="100%"></iframe>
 			</div>
 		</>
 	);
